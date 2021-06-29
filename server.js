c1ef51a488e1bdb06a99ee4a7b1a4687cd6ed412
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-28 23:52:50 -0600

    Added the rest of the server configuration

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const expHBS = require('express-handlebars');
+
+app.engine('.hbs', expHBS({
+   defaultLayout: 'main',
+   extname: '.hbs'
+}))
+
+app.set('view engine', 'hbs');
+
+const routes = require('./controllers/controller.js');
+app.use(routes);
+
+app.listen(port, () => {
+   console.log('Listening on port ' + port);
+});
